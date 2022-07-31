39bfef9a35aa0fc617b69b7ea226403beb6d5fe2
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-07-31 13:05:55 -0600

    Enable the functionality to get the country for backend for register process.

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,7 +1,9 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CustomPopUpService } from 'src/app/shared/components/custom-pop-up/custom-pop-up.service';
 import { AuthenticationService } from 'src/app/shared/http-services/authentication/authentication.service';
+import { Countries } from 'src/app/shared/interfaces/countries';
+import { CountriesService } from 'src/app/shared/http-services/utils/countries.service';
 
 
 @Component({
@@ -9,7 +11,7 @@ import { AuthenticationService } from 'src/app/shared/http-services/authenticati
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
   signUpForm = new FormGroup({
     id: new FormControl('', {
@@ -41,27 +43,52 @@ export class RegisterComponent {
     }),
     password: new FormControl('', {
       validators: Validators.required
+    }),
+    country: new FormControl(0, {
+      validators: Validators.required
     })
   });
 
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
+  selectedCountry = 0;
+  countries: Countries[] = [];
+
 
   constructor(
     private customPopUpService: CustomPopUpService,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private countriesService: CountriesService
   ) { }
+
+  ngOnInit(): void {
+    this.countriesService.getCountries().subscribe(
+      data => {
+        if (data.length > 0){
+          this.countries = data
+        } 
+      },
+      err => {
+        if (err.status !== 200){
+          this.openCustomPopUp('Hubo un error en el servidor, contacte a los administradores.');
+        } 
+      }
+    )
+  }
 
   public openCustomPopUp(message: string) {
     this.customPopUpService.confirm(
-      'New user', 
+      'Nuevo usuario', 
       message
       );
   }
 
-  onSubmit() {
+  chooseCountry() {
+    this.signUpForm.controls['country'].setValue(this.selectedCountry);
+  }
 
+  onSubmit() {
     console.log(this.signUpForm.controls['birthDate'].value!.toISOString())
 
     this.authService.register(
@@ -74,7 +101,7 @@ export class RegisterComponent {
       this.signUpForm.controls['phone'].value!,
       this.signUpForm.controls['password'].value!,
       this.signUpForm.controls['birthDate'].value!.toISOString(),
-      0).subscribe(
+      this.signUpForm.controls['country'].value!).subscribe(
       data => {
         console.log(data)
         this.isSuccessful = true;
