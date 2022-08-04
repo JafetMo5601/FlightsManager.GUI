927af9e91d5a009713187b313a63097db9d63b8c
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-08-03 20:12:51 -0600

    Implement the mockup of the reservation details.

--- src/app/dashboard/reservation-details/reservation-details.component.ts
+++ src/app/dashboard/reservation-details/reservation-details.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reservation-details.component.css']
 })
 export class ReservationDetailsComponent implements OnInit {
+  arrivalCountry: string = 'Lima, Peru'
+  departureCountry: string = 'San Jose, Costa Rica'
+  orderNumber: string = 'LL09123A'
+  arrivalDate: string = '2022-08-25 07:00:00.0000000'
+  departureDate: string = '2022-08-25 08:00:00.0000000'
 
   constructor() { }
 
