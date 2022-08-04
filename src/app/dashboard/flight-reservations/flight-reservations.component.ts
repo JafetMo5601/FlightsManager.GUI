fbdc280d420fd0b74d3979dbe572cc96a37b9956
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-08-03 20:11:52 -0600

    Enable the functionality to retrieve info from the backend

--- src/app/dashboard/flight-reservations/flight-reservations.component.ts
+++ src/app/dashboard/flight-reservations/flight-reservations.component.ts
@@ -4,11 +4,12 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs/internal/Observable';
 import { startWith, map } from 'rxjs/operators';
 import { CustomPopUpService } from 'src/app/shared/components/custom-pop-up/custom-pop-up.service';
-import { CountriesService } from 'src/app/shared/http-services/utils/countries.service';
-import { Asientos } from 'src/app/shared/interfaces/asientos';
+import { CountriesService } from 'src/app/shared/http-services/utils/countries.service'
+import { FlightsService } from 'src/app/shared/http-services/utils/flights.service';
 import { Countries } from 'src/app/shared/interfaces/countries';
 import { Horario } from 'src/app/shared/interfaces/horario';
 import { Tarifa } from 'src/app/shared/interfaces/tarifa';
+import { Vuelo } from 'src/app/shared/interfaces/vuelo';
 
 
 @Component({
@@ -17,80 +18,27 @@ import { Tarifa } from 'src/app/shared/interfaces/tarifa';
   styleUrls: ['./flight-reservations.component.css']
 })
 export class FlightReservationsComponent implements OnInit {
-  // displayedColumns: string[] = ['aeropuerto', 'avion', 'clase', 'precio', 'salidaPais'];
-  // dataSource = new MatTableDataSource(ELEMENT_DATA);
-
-  // applyFilter(event: Event){
-  //   const filterValue = (event.target as HTMLInputElement).value;
-  //   this.dataSource.filter = filterValue.trim().toLowerCase();
-  // }
-
-  selectedClass = 0;
+  paisPartida: FormControl<string | null> = new FormControl('');
+  paisLlegada: FormControl<string | null> = new FormControl('');
+  paises: Countries[] = [];
+  paisesFiltered: Countries[] = [];
+  filteredOptions: Observable<Countries[]> | undefined = undefined;
+  filteredAgainOptions: Observable<Countries[]> | undefined = undefined;
+  displayedColumns: string[] = ['airport_departure', 'airport_arrival', 'avion', 'class', 'price'];
+  selectedClass: number = 0;
+  selectedSchedule: number = 0;
   
-  tarifas: Tarifa[] = [
-    {  
-      id: 1,
-      clase: 'Turista',
-      precio: 200, 
-      asiento: {
-        id: 1,
-        letra: 'A',
-        fila: '3'
-      }
-    },
-    {  
-      id: 2,
-      clase: 'Ejecutivo',
-      precio: 300, 
-      asiento: {
-        id: 1,
-        letra: 'A',
-        fila: '3'
-      }
-    }
-  ];
+  tarifas: Tarifa[] = [];
+  horarios: Horario[] = [];
 
-  horarios: Horario[] = [
-    {
-      id: 1,
-      horaPartida: "2022-08-25T07:00:00",
-      horaLlegada: "2022-08-25T09:30:00"
-    },
-    {
-      id: 2,
-      horaPartida: "2022-08-25T08:00:00",
-      horaLlegada: "2022-08-25T10:10:00"
-    },
-    {
-      id: 3,
-      horaPartida: "2022-08-25T09:00:00",
-      horaLlegada: "2022-08-25T10:30:00"
-    },
-    {
-      id: 4,
-      horaPartida: "2022-08-25T10:00:00",
-      horaLlegada: "2022-08-25T11:30:00"
-    },
-    {
-      id: 5,
-      horaPartida: "2022-08-25T11:00:00",
-      horaLlegada: "2022-08-25T12:30:00"
-    },
-  ]
+  flights: Vuelo[] = [];
+  dataSource = new MatTableDataSource(this.flights);
 
   constructor(
     private customPopUpService: CustomPopUpService,
-    private countryService: CountriesService
+    private countryService: CountriesService,
+    private flightService: FlightsService
   ) { }
-
-  paisPartida = new FormControl('');
-  paisLlegada = new FormControl('');
-
-  paises: Countries[] = [];
-  paisesFiltered: Countries[] = [];
-  
-  filteredOptions: Observable<Countries[]> | undefined = undefined;
-  filteredAgainOptions: Observable<Countries[]> | undefined = undefined;
 
   ngOnInit() {
     this.countryService.getCountries().subscribe(
@@ -99,6 +47,32 @@ export class FlightReservationsComponent implements OnInit {
       },
       err => {
         this.openCustomPopUp('Hubo un error obteniendo los paises, contacte a los administradores.');
+      }
+    )
+
+    this.flightService.getNextFlights().subscribe(
+      data => {
+        console.log(data);
+        this.dataSource = new MatTableDataSource(data);
+      },
+      err => {}
+    )
+
+    this.flightService.getHorarios().subscribe(
+      data => {
+        this.horarios = data;
+      },
+      err => {
+        this.openCustomPopUp('Hubo un error obteniendo los horarios, contacte a los administradores.');
+      }
+    )
+
+    this.flightService.getTarifas().subscribe(
+      data => {
+        this.tarifas = data;
+      },
+      err => {
+        this.openCustomPopUp('Hubo un error obteniendo las tarifas, contacte a los administradores.');
       }
     )
     
@@ -111,6 +85,13 @@ export class FlightReservationsComponent implements OnInit {
       startWith(''),
       map(value => this._secondFilter(value || '')),
     );
+  }
+
+  printValues() {
+    console.log(this.paisLlegada.getRawValue());
+    console.log(this.paisPartida.getRawValue());
+    console.log(this.selectedSchedule);
+    console.log(this.selectedClass);
   }
 
   private _filter(value: string): Countries[] {
