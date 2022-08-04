f240c8b36b61c5f73d996bee63e7d0691d17e863
JafetMo5601 <jafet.moraugalde15@gmail.com>
2022-08-03 20:13:31 -0600

    Retrieve the information of the flights from the backend and delete the hardcoded data.

--- src/app/dashboard/next-flights/next-flights.component.ts
+++ src/app/dashboard/next-flights/next-flights.component.ts
@@ -1,53 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { CustomPopUpService } from 'src/app/shared/components/custom-pop-up/custom-pop-up.service';
+import { FlightsService } from 'src/app/shared/http-services/utils/flights.service';
 import { Vuelo } from 'src/app/shared/interfaces/vuelo';
 
-
-const ELEMENT_DATA: Vuelo[] = [
-  {
-    id: 1, 
-    avion: {
-      id: 1,
-      fabricante: 'Fabricante',
-      tipo: 'Prueba',
-      capacidad: 200,
-      aerolinea: {
-        id: 1,
-        nombre: 'Aerolinea' 
-      }
-    }, 
-    tarifa: {
-      id: 1,
-      clase: 'Turista',
-      precio: 200,
-      asiento: {
-        id: 1,
-        letra: 'A',
-        fila: '3'
-      },
-    }, 
-    reserva: {
-      id: 1,
-      costo: 200,
-      fecha: 'Fecha',
-      observacion: 'Una prueba'
-    },
-    aeropuerto: {
-      id: 1,
-      nombre: 'Aeropuerto',
-      pais: {
-        id: 1,
-        nombre: 'Costa Rica'
-      },
-    },
-    horario: {
-      id: 1,
-      horaPartida: '',
-      horaLlegada: ''
-    }
-
-  }
-];
 
 @Component({
   selector: 'app-next-flights',
@@ -55,18 +11,35 @@ const ELEMENT_DATA: Vuelo[] = [
   styleUrls: ['./next-flights.component.css']
 })
 export class NextFlightsComponent implements OnInit {
+  displayedColumns: string[] = ['airport_departure', 'airport_arrival', 'avion', 'class', 'price'];
+  flights: Array<Vuelo> = []
+  dataSource = new MatTableDataSource(this.flights);
 
-  constructor() { }
+  constructor(
+    private customPopUpService: CustomPopUpService,
+    private flightService: FlightsService
+  ) { }
 
   ngOnInit(): void {
+    this.flightService.getNextFlights().subscribe(
+      data => {
+        console.log(data);
+        this.dataSource = new MatTableDataSource(data);
+      },
+      err => {}
+    )
   }
-
-  displayedColumns: string[] = ['aeropuerto', 'avion', 'clase', 'precio', 'salidaPais'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  
 
   applyFilter(event: Event){
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  public openCustomPopUp(message: string) {
+    this.customPopUpService.confirm(
+      'Reservaciones', 
+      message
+      );
+  }
 }
